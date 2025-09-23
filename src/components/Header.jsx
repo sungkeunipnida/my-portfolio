ddae2c9cdb211ecb719f1a2cd12759b2a23fafdc
sungkeunipnida <iorumer@gmail.com>
2025-09-23 19:06:09 +0900

    headerText

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-800/80 backdrop-blur-sm text-white p-4 flex justify-between items-center shadow z-50">
-      <h1 className="text-xl font-bold">My Portfolio</h1>
+      <h1 className="text-xl font-bold">sungkeun</h1>
       <nav className="space-x-4">
         <a href="#home" className="hover:text-blue-400">
           Home
