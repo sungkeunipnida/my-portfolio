2ca7f83216cf35d659b3577c165c219011fa36e7
sungkeunipnida <iorumer@gmail.com>
2025-09-23 17:32:18 +0900

    text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,8 @@ function Contact() {
   return (
     <section id="contact" className="p-8 md:p-16 bg-gray-100">
       <h2 className="text-2xl font-bold mb-4">Contact</h2>
-      <p>📧 이메일: your.email@example.com</p>
-      <p>💻 GitHub: github.com/yourusername</p>
+      <p>📧 Email iormer@gmail.com</p>
+      <p>💻 GitHub: github.com/sungkeunipnida</p>
     </section>
   );
 }
